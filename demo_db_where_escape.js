dd62bdc637f11feb0e92f851250228ed9aeef030
Katriin Poolakese <katriin.poolaese@khk.ee>
2022-02-21 09:29:12 +0200

    Where

---
+++ demo_db_where_escape.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "qwerty",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  var name = 'Amy';
+  var adr = 'Mountain 21';
+  var sql = 'SELECT * FROM customers WHERE name = ? OR address = ?';
+  con.query(sql, [name, adr], function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
